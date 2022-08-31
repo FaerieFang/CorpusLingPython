a5f24c80e3b891fd3b0d1d76a466d936ce254e45
Alexis Schlagenhauf <faerieclique@gmail.com>
2022-08-31 15:22:57 -0600

    homework1 complete

--- SchlagenhaufHomework1.py
+++ SchlagenhaufHomework1.py
@@ -1,11 +1,15 @@
+import pprint
 import nltk
-sentence = input("enter corpus sentence:")
+with open('data.txt', 'r') as file:
+    data = file.read().replace('\n', '')
+sentence = data
 sentence = str.lower(sentence)
 punc = ".!()[]{}:;,?"
 for ele in sentence:
     if ele in punc:
         sentence = sentence.replace(ele, "")
 tokens = nltk.word_tokenize(sentence)
-print(tokens)
+
 occurrence = {item: tokens.count(item) for item in tokens}
-print(occurrence)
+sortedTokens = sorted(occurrence.items(), key = lambda x: x[1], reverse = True)
+pprint.pprint(sortedTokens)
